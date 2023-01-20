23964c198c36930e9445cce486d82bc5325f2990
Vedant Jain <85900614+vedantj2@users.noreply.github.com>
2023-01-20 02:22:42 -0800

    js

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const flightPath =  {
     curviness: 1.25,
     autoRotate: true,
     values: [
-        {x:100, y:-20},
-        {x:300, y:10},
-        {x:500, y:100},
-        {x:750, y:-100},
-        {x:350, y:-50},
-        {x:600, y:100},
-        {x:800, y:0},
-        {x:window.innerWidth, y:-200},
+        {x:1400, y:0},
+        {x:1000, y:20},
+      
+        // {x:window.innerWidth, y:-200},
     ]
 };
 
